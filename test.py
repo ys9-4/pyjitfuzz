1941c10f14846df22baf16963be1827138f4b0a2
ys9-4 <jys5440@gmail.com>
2025-05-01 16:42:06 +0900

    edit: state func

--- test.py
+++ test.py
@@ -1,13 +1,20 @@
-def test(a, b):
-    c = a + b
-    state()
-    return c
+import math
+
+def something():
+    result = 1
+    return result
 
 
-def test2(a, b):
-    c = a[0] + b[0]
-    state()
-    return c
+def main():
+    result = 0
+    
+    for i in range(0, 10000):
+        print(probe_state(result))
+        result = something()
+        print(probe_state(result))
+        
+    save_state() # where to save?
 
-test(1, 2)
-test2([1, 1, 1], [2, 2, 2])
+
+if __name__ == "__main__":
+    main()
